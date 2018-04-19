127cef3df87b7b00912c73f3b61f1c01da60bae3
Jack Forrest <jack@jrforrest.net>
2018-04-19 06:28:23 -0600

    Corrects SVM, optimizes RF and MLP
    SVM was misclasifying all as type 0 due to lack of scaling in input
    feature.  Here we apply a quick minmax scaler.  This doesn't affect
    accuracy for the other models yet, and MLP still outperforms SVM.
    
    Tuned MLP a bit with more iterations and forcing hidden layers at a more
    sane default.
    
    RF needed more estimators and a more sane sample split it looks like but
    I think I optimized us into a local minima here.
    
    Anyway, results are still dismial, I need a deeper understanding of the
    dataset to try to optimize further probably.  Histogram visualization
    per-class is probably a solid next move here.

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ import os
 import re
 import pandas
 
-from sklearn import model_selection
-from sklearn import svm
-from sklearn import ensemble
-from sklearn import metrics
-from sklearn import naive_bayes
-from sklearn import neural_network
+from sklearn import model_selection, svm, ensemble, metrics, naive_bayes, neural_network, preprocessing
 
 NLCD_CODES = ["DF", "DHI", "EF", "OW", "PH", "SS"]
 
@@ -22,8 +17,14 @@ class DataSet:
 
     def get_features(self):
         band_keys = [key for key in list(self.data_frame) if re.search("B\d+", key)]
-
         return self.data_frame[band_keys]
+
+    def get_scaled_features(self):
+        scaler = preprocessing.StandardScaler()
+        return scaler.fit_transform(self.get_features())
+
+    def scale_features():
+        scaler = preprocessing.StandardScaler
 
     def split_samplings(self, test_size):
         train, test = model_selection.train_test_split(
@@ -33,33 +34,51 @@ class DataSet:
 
 class Trainer:
     def __init__(self, data_set):
+        self.data_set = data_set
         self.training_set, self.test_set = data_set.split_samplings(0.2)
+        self.scaler = preprocessing.MinMaxScaler()
         self.models = {
             'svc': svm.SVC(),
-            'rf': ensemble.RandomForestClassifier(),
+            'rf': ensemble.RandomForestClassifier(
+                n_estimators = 50,
+                min_samples_split = 0.25,
+                max_features = "sqrt"),
             'gnb': naive_bayes.GaussianNB(),
-            'mlp': neural_network.MLPClassifier()
+            'mlp': neural_network.MLPClassifier(max_iter = 2000, hidden_layer_sizes = [100])
         }
         self.is_fit = False
 
     def fit(self):
         if not self.is_fit:
+            self.scaler.fit(self.data_set.get_features())
+
+            scaled_training_features = self.scaler.transform(self.training_set.get_features())
+
+            import ipdb; ipdb.set_trace()
+
             for model in self.models.values():
-                model.fit(
-                    self.training_set.get_features(),
-                    self.training_set.get_classes())
+                model.fit(scaled_training_features, self.training_set.get_classes())
+
             self.is_fit = True
 
     def accuracies(self):
         return {k: self.accuracy_for_model(k) for k in self.models.keys()}
 
-    def accuracy_for_model(self, name):
-        return metrics.accuracy_score(
-            self.test_set.get_classes(),
-            self.results_for_model(name))
+    def accuracies_on_trained(self):
+        return {k: self.accuracy_for_model(k, self.training_set) for k in self.models.keys()}
 
-    def results_for_model(self, name):
-        return self.models[name].predict(self.test_set.get_features())
+    def accuracy_for_model(self, name, test_set = None):
+        test_set = test_set or self.test_set
+
+        return metrics.accuracy_score(
+            test_set.get_classes(),
+            self.results_for_model(name, test_set = test_set))
+
+    def results_for_model(self, name, test_set = None):
+        test_set = test_set or self.test_set
+        scaled_test_features = self.scaler.transform(test_set.get_features())
+
+        return self.models[name].predict(scaled_test_features)
 
 def load_data_class(name):
     current_dir = os.path.dirname(__file__)
@@ -87,6 +106,7 @@ def main():
     trainer = build_trainer()
     trainer.fit()
     print(trainer.accuracies())
+    print(trainer.accuracies_on_trained())
 
 if __name__ == "__main__":
     main()
